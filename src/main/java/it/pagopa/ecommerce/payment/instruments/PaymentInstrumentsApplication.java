cbd26b886e7a0fc87c920c3b665100e03868ccb2
MarcoAGreco <marcoandreagreco@gmail.com>
2022-05-31 15:12:48 +0200

    [CHK-449] update pom, add scheduled task

--- src/main/java/it/pagopa/ecommerce/payment/instruments/PaymentInstrumentsApplication.java
+++ src/main/java/it/pagopa/ecommerce/payment/instruments/PaymentInstrumentsApplication.java
@@ -2,8 +2,10 @@ package it.pagopa.ecommerce.payment.instruments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PaymentInstrumentsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/it/pagopa/ecommerce/payment/instruments/task/ScheduledPSPUpdate.java
@@ -0,0 +1,19 @@
+package it.pagopa.ecommerce.payment.instruments.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class ScheduledPSPUpdate {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(cron = "0 0 * * *")
+    public void updatePSPs(){
+        log.info("Starting PSPs scheduled update. Time: {}", dateFormat.format(new Date()));
+    }
+}
