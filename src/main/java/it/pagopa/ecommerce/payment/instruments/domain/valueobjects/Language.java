2988438ba2d658dd1e7e27ad90c0f10dfa4432da
MarcoAGreco <marcoandreagreco@gmail.com>
2022-06-03 16:49:01 +0200

    [CHK-446] Add Psp repository, enum type

---
+++ src/main/java/it/pagopa/ecommerce/payment/instruments/domain/valueobjects/Language.java
@@ -0,0 +1,24 @@
+package it.pagopa.ecommerce.payment.instruments.domain.valueobjects;
+
+import it.pagopa.ecommerce.payment.instruments.utils.LanguageEnum;
+import lombok.EqualsAndHashCode;
+import org.springframework.lang.NonNull;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@ValueObjects
+@EqualsAndHashCode
+public class Language implements Serializable {
+    private final LanguageEnum language;
+
+    public Language(@NonNull LanguageEnum language) {
+
+        this.language = Objects.requireNonNull(language);
+    }
+
+    public @NonNull LanguageEnum value() {
+
+        return language;
+    }
+}
---
+++ src/main/java/it/pagopa/ecommerce/payment/instruments/infrastructure/PspRepository.java
@@ -0,0 +1,8 @@
+package it.pagopa.ecommerce.payment.instruments.infrastructure;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+
+public interface PspRepository extends ReactiveCrudRepository<PspDocument, String> {
+    Flux<PspDocument> findByPSPName(String businessName);
+}
