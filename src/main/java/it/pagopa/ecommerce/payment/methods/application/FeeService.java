de0fd524717ba61361583095f1c5a0a997a12e63
MarcoAGreco <marcoandreagreco@gmail.com>
2023-03-01 17:16:21 +0100

    [CHK-883] feat: add fee controller

---
+++ src/main/java/it/pagopa/ecommerce/payment/methods/application/FeeService.java
@@ -0,0 +1,59 @@
+package it.pagopa.ecommerce.payment.methods.application;
+
+import it.pagopa.ecommerce.payment.methods.client.AfmClient;
+import it.pagopa.ecommerce.payment.methods.domain.aggregates.PspFactory;
+import it.pagopa.ecommerce.payment.methods.infrastructure.PaymentMethodRepository;
+import it.pagopa.ecommerce.payment.methods.server.model.PaymentOptionDto;
+import it.pagopa.ecommerce.payment.methods.utils.ApplicationService;
+import it.pagopa.generated.ecommerce.gec.v1.dto.BundleOptionDto;
+import it.pagopa.generated.ecommerce.gec.v1.dto.TransferListItemDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.stream.Collectors;
+
+@Service
+@ApplicationService
+@Slf4j
+public class FeeService {
+    @Autowired
+    private PspFactory pspFactory;
+
+    @Autowired
+    private AfmClient afmClient;
+
+    @Autowired
+    private PaymentMethodRepository paymentMethodRepository;
+
+    public Mono<BundleOptionDto> computeFee(
+                                            Mono<PaymentOptionDto> paymentOptionDto,
+                                            int maxOccurrences
+    ) {
+        return paymentOptionDto.map(
+                po -> new it.pagopa.generated.ecommerce.gec.v1.dto.PaymentOptionDto()
+                        .bin(po.getBin())
+                        .paymentAmount(po.getPaymentAmount())
+                        .idPspList(po.getIdPspList())
+                        .paymentMethod(po.getPaymentMethod())
+                        .primaryCreditorInstitution(po.getPrimaryCreditorInstitution())
+                        .touchpoint(po.getTouchpoint())
+                        .transferList(
+                                po.getTransferList()
+                                        .stream()
+                                        .map(
+                                                t -> new TransferListItemDto()
+                                                        .creditorInstitution(t.getCreditorInstitution())
+                                                        .digitalStamp(t.getDigitalStamp())
+                                                        .transferCategory(t.getTransferCategory())
+                                        )
+                                        .collect(Collectors.toList())
+                        )
+        ).flatMap(reqBody -> {
+            var ret = afmClient.getFees(reqBody, maxOccurrences);
+            log.debug("Retval: " + ret);
+            return ret;
+        });
+    }
+}
---
+++ src/main/java/it/pagopa/ecommerce/payment/methods/controller/FeeController.java
@@ -0,0 +1,30 @@
+package it.pagopa.ecommerce.payment.methods.controller;
+
+import it.pagopa.ecommerce.payment.methods.application.FeeService;
+import it.pagopa.ecommerce.payment.methods.server.api.FeeApi;
+import it.pagopa.ecommerce.payment.methods.server.model.BundleOptionDto;
+import it.pagopa.ecommerce.payment.methods.server.model.PaymentOptionDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@RestController
+@Slf4j
+public class FeeController implements FeeApi {
+
+    @Autowired
+    FeeService feeService;
+
+    @Override
+    public Mono<ResponseEntity<BundleOptionDto>> calculateFees(
+                                                               Mono<PaymentOptionDto> paymentOptionDto,
+                                                               Integer maxOccurrences,
+                                                               ServerWebExchange exchange
+    ) {
+        feeService.computeFee(paymentOptionDto, maxOccurrences);
+        return null;
+    }
+}
