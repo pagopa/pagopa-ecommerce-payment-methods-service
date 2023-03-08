810d5edc2bc63feaf8ab5105731589a63ddb99cc
Fernando-Granato <fernando.granato@pagopa.it>
2023-03-08 10:17:24 +0100

    Feat: removed import

--- src/main/java/it/pagopa/ecommerce/payment/methods/exception/PyamentMethodNotFoundException.java
+++ src/main/java/it/pagopa/ecommerce/payment/methods/exception/PyamentMethodNotFoundException.java
@@ -1,7 +1,5 @@
 package it.pagopa.ecommerce.payment.methods.exception;
 
-import it.pagopa.ecommerce.payment.methods.domain.valueobjects.PaymentMethodName;
-
 public class PyamentMethodNotFoundException extends RuntimeException {
 
     public PyamentMethodNotFoundException(String paymentMethodId) {
