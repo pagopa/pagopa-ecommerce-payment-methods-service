bf438e3833837c6ce68b9675fb98b6851329011d
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2023-09-29 10:12:25 +0200

    feat(deps): update commons version (#108)
    * feat(deps): update commons version
    
    * feat(deps): update commons version
    
    * feat(deps): update commons version
    
    * fix: update commons version to latest

--- src/test/java/it/pagopa/ecommerce/payment/methods/client/NpgWebClientConfigTest.java
+++ src/test/java/it/pagopa/ecommerce/payment/methods/client/NpgWebClientConfigTest.java
@@ -1,9 +1,11 @@
 package it.pagopa.ecommerce.payment.methods.client;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.opentelemetry.api.trace.Tracer;
 import it.pagopa.ecommerce.commons.client.NpgClient;
 import it.pagopa.ecommerce.commons.generated.npg.v1.ApiClient;
 import it.pagopa.ecommerce.commons.generated.npg.v1.api.PaymentServicesApi;
+import it.pagopa.ecommerce.commons.queues.StrictJsonSerializerProvider;
 import it.pagopa.ecommerce.payment.methods.config.NpgWebClientsConfig;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -11,11 +13,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static it.pagopa.ecommerce.commons.queues.StrictJsonSerializerProvider.OBJECT_MAPPER;
-
 @ExtendWith(MockitoExtension.class)
 class NpgWebClientConfigTest {
     private final Tracer tracer = Mockito.mock(Tracer.class);
+
+    private final ObjectMapper objectMapper = new StrictJsonSerializerProvider().getObjectMapper();
 
     @Test
     void testNpgWebClientConfigApi() {
@@ -29,7 +31,7 @@ class NpgWebClientConfigTest {
     void testNpgWebClientConfigNpgClient() {
         NpgWebClientsConfig config = new NpgWebClientsConfig();
         PaymentServicesApi api = config.npgWebClient("localhost/test", 10000, 10000);
-        NpgClient npgClient = config.npgClient(api, tracer, OBJECT_MAPPER);
+        NpgClient npgClient = config.npgClient(api, tracer, objectMapper);
         Assert.assertNotNull(npgClient);
         Assert.assertEquals(NpgClient.class, npgClient.getClass());
     }
