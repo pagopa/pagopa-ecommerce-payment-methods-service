313ca02da69e25b5562df29a55d3721a2c473cf6
Fernando-Granato <fernando.granato@pagopa.it>
2023-11-08 12:33:27 +0100

    feat: added logic for execute orderId generation

--- src/main/java/it/pagopa/ecommerce/payment/methods/infrastructure/UniqueIdDocument.java
+++ src/main/java/it/pagopa/ecommerce/payment/methods/infrastructure/UniqueIdDocument.java
@@ -1,24 +1,8 @@
 package it.pagopa.ecommerce.payment.methods.infrastructure;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.PersistenceConstructor;
-import org.springframework.data.redis.core.RedisHash;
 import org.springframework.lang.NonNull;
 
-@RedisHash(value = "keys")
 public record UniqueIdDocument(@NonNull @Id String id) {
-    /*
-     * @formatter:off
-     *
-     * Warning java:S6207 - Redundant constructors/methods should be avoided in records
-     * Suppressed because this constructor is just to add the `@PersistenceConstructor` annotation
-     * and is currently the canonical way to add annotations to record constructors
-     *
-     * @formatter:on
-     */
-    @SuppressWarnings("java:S6207")
-    @PersistenceConstructor
-    public UniqueIdDocument {
-        // Do nothing
-    }
+
 }
--- src/main/java/it/pagopa/ecommerce/payment/methods/utils/UniqueIdUtils.java
+++ src/main/java/it/pagopa/ecommerce/payment/methods/utils/UniqueIdUtils.java
@@ -1,16 +1,44 @@
 package it.pagopa.ecommerce.payment.methods.utils;
 
+import it.pagopa.ecommerce.payment.methods.exception.UniqueIdGenerationException;
+import it.pagopa.ecommerce.payment.methods.infrastructure.UniqueIdDocument;
+import it.pagopa.ecommerce.payment.methods.infrastructure.UniqueIdTemplateWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 import java.security.SecureRandom;
+import java.time.Duration;
 
 @Component
 public class UniqueIdUtils {
+    private final UniqueIdTemplateWrapper uniqueIdTemplateWrapper;
     private static final SecureRandom secureRandom = new SecureRandom();
     private static final String ALPHANUMERICS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-._";
     private static final int MAX_LENGTH = 18;
+    private static final int MAX_NUMBER_ATTEMPTS = 3;
 
-    public String generateUniqueId() {
+    @Autowired
+    public UniqueIdUtils(UniqueIdTemplateWrapper uniqueIdTemplateWrapper) {
+        this.uniqueIdTemplateWrapper = uniqueIdTemplateWrapper;
+    }
+
+    public Mono<String> generateUniqueId() {
+        boolean isSuccessfullySaved = false;
+        int index = 0;
+        String uniqueId = generateRandomIdentifier();
+        while (index <= MAX_NUMBER_ATTEMPTS && !isSuccessfullySaved) {
+            isSuccessfullySaved = uniqueIdTemplateWrapper
+                    .saveIfAbsent(new UniqueIdDocument(uniqueId), Duration.ofSeconds(60));
+            index++;
+            if (!isSuccessfullySaved) {
+                uniqueId = generateRandomIdentifier();
+            }
+        }
+        return isSuccessfullySaved ? Mono.error(new UniqueIdGenerationException()) : Mono.just(uniqueId);
+    }
+
+    private static String generateRandomIdentifier() {
         String timestampToString = String.valueOf(System.currentTimeMillis());
         int randomStringLength = MAX_LENGTH - timestampToString.length();
         return timestampToString + generateRandomString(randomStringLength);
