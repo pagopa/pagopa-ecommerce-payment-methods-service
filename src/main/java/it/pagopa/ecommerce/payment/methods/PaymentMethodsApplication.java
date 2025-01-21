c19e2dddd4928087a5408e7c070c353fa534d843
Dylan Tangredi <dylan.tangredi@bvtech.com>
2025-01-21 13:07:29 +0100

    [PIDM-44] fix(scheduling): remove EnableScheduling annotation

--- src/main/java/it/pagopa/ecommerce/payment/methods/PaymentMethodsApplication.java
+++ src/main/java/it/pagopa/ecommerce/payment/methods/PaymentMethodsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableConfigurationProperties(SessionUrlConfig.class)
 public class PaymentMethodsApplication {
 
