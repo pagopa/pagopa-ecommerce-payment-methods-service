922d298da67e47e5672bb50c091856c944a1628e
forciniti-luigi-jakala <luigi.forciniti.ext@jakala.com>
2025-06-20 12:53:09 +0200

    chore:[PIDM-559-564-upgrade-springboot3-ecommerce-payment-methods-service] upgrade Spring Boot from 2.6.7 to 3.4.5 (#255)
    * chore: upgrade Spring Boot from 2.6.7 to 3.4.5
    
    * chore:  spotless:apply
    
    * chore:  add @MockBean for test
    
    * chore: replace spring.redis.* with spring.data.redis.* in test
    
    * chore: replace spring.redis.* with spring.data.redis.* in test
    
    * chore: replace in Dockerfile "org.springframework.boot.loader.JarLauncher"
     with "org.springframework.boot.loader.launch.JarLauncher"
    
    * chore: code review
    
    * chore: update env example
    
    * chore: update commons library ref
    
    ---------
    
    Co-authored-by: Forciniti Luigi <forciniti@spi.srl>
    Co-authored-by: Pietro Tota <pietro.tota@pagopa.it>
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>

--- src/main/java/it/pagopa/ecommerce/payment/methods/PaymentMethodsApplication.java
+++ src/main/java/it/pagopa/ecommerce/payment/methods/PaymentMethodsApplication.java
@@ -4,7 +4,6 @@ import it.pagopa.ecommerce.payment.methods.config.SessionUrlConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SessionUrlConfig.class)
